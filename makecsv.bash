c9e4fa5649a645bae5f4c8608868a3036dd77ebb
MyMac <contact@econpoint.com>
2018-02-10 11:16:25 -0500

    truncation of title and updated country codes

--- makecsv.bash
+++ makecsv.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script will run for about 2 hours
+# this script will run for about 1.5-2 hours
 
 # download the data manually (change to the directory containing the downloaded file before running this script)
 
@@ -13,5 +13,5 @@ mkdir -p orciddata
 
 # the first command untars just the json files and pipes them (to avoid extracting millions of individual files) to jq, which extracts just the information of interest and stores it in csv format
 echo "orcid,country,yearstart,yearend,type,roletitle" > "$outfile" 
-tar -xf "$rawfile" -O | jq -rc '."orcid-identifier".path as $id|."activities-summary".educations|."education-summary"| if .==null then empty else .[] end| [$id,(.organization.address.country),."start-date"."year".value,."end-date".year.value,"EDUCATION",(."role-title"|if .==null then "NA:::n-u-l-l" else (.|gsub("\r";"")|gsub("\n";"")|.[0:100]) end)]|@csv' >> "$outfile"
-tar -xf "$rawfile" -O | jq -rc '."orcid-identifier".path as $id|."activities-summary".employments|."employment-summary"| if .==null then empty else .[] end| [$id,(.organization.address.country),."start-date"."year".value,."end-date".year.value,"EMPLOYMENT",(."role-title"|if .==null then "NA:::n-u-l-l" else (.|gsub("\r";"")|gsub("\n";"")|.[0:100]) end)]|@csv' >> "$outfile"
+tar -xf "$rawfile" -O | jq -rc '."orcid-identifier".path as $id|."activities-summary".educations|."education-summary"| if .==null then empty else .[] end| [$id,(.organization.address.country),."start-date"."year".value,."end-date".year.value,"EDUCATION",(."role-title"|if .==null then "NA:::n-u-l-l" else (.|gsub("\r";"")|gsub("\n";"")|.[0:50]) end)]|@csv' >> "$outfile"
+tar -xf "$rawfile" -O | jq -rc '."orcid-identifier".path as $id|."activities-summary".employments|."employment-summary"| if .==null then empty else .[] end| [$id,(.organization.address.country),."start-date"."year".value,."end-date".year.value,"EMPLOYMENT",(."role-title"|if .==null then "NA:::n-u-l-l" else (.|gsub("\r";"")|gsub("\n";"")|.[0:50]) end)]|@csv' >> "$outfile"
