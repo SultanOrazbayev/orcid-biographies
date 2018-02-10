c5dbc85af6f4e72a522184947385bbc022c3ed3a
MyMac <contact@econpoint.com>
2018-02-10 01:33:18 -0500

    updated to 2017 public file

--- makecsv.bash
+++ makecsv.bash
@@ -5,10 +5,13 @@
 # download the data manually (change to the directory containing the downloaded file before running this script)
 
 # change the raw file name below IF the file was re-named after downloading
-rawfile="ORCID_public_data_file_2016.tar"
+rawfile="public_profiles_API2.0_2017_10_json.tar.gz"
+outfile="orciddata/orcid-raw.csv"
 
 # this command creates a folder which will store the processed file
-mkdir orciddata
+mkdir -p orciddata
 
 # the first command untars just the json files and pipes them (to avoid extracting millions of individual files) to jq, which extracts just the information of interest and stores it in csv format
-tar -xf "$rawfile" --exclude="xml" --include="*.json" -O | jq -rc '."orcid-profile"|."orcid-identifier".path as $id|."orcid-activities"| if .==null then empty else .affiliations end| if .==null then empty else .affiliation end| if type=="array" then .[] else . end|[$id,."start-date"."year".value,."end-date".year.value,.type,(."role-title"|if .==null then . else (.|gsub("\r";"")|gsub("\n";"")) end),.organization.address.country]|@csv' > "orciddata/orcid-raw.csv"
+echo "orcid,country,yearstart,yearend,type,roletitle" > "$outfile" 
+tar -xf "$rawfile" -O | jq -rc '."orcid-identifier".path as $id|."activities-summary".educations|."education-summary"| if .==null then empty else .[] end| [$id,(.organization.address.country),."start-date"."year".value,."end-date".year.value,"EDUCATION",(."role-title"|if .==null then "NA:::n-u-l-l" else (.|gsub("\r";"")|gsub("\n";"")|.[0:100]) end)]|@csv' >> "$outfile"
+tar -xf "$rawfile" -O | jq -rc '."orcid-identifier".path as $id|."activities-summary".employments|."employment-summary"| if .==null then empty else .[] end| [$id,(.organization.address.country),."start-date"."year".value,."end-date".year.value,"EMPLOYMENT",(."role-title"|if .==null then "NA:::n-u-l-l" else (.|gsub("\r";"")|gsub("\n";"")|.[0:100]) end)]|@csv' >> "$outfile"
